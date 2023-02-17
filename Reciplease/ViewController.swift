f9b21ca4401c139fa34a84d3b54fa1abadb10dda
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-02-17 16:19:37 +0100

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Wass on 17/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
