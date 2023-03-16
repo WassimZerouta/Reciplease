6820ad36c673d90a625d5e8405d2dcbc94f50687
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-03-16 10:16:12 +0100

    Add cell gradient

--- Reciplease/Controllers/RecipeTableViewCell.swift
+++ Reciplease/Controllers/RecipeTableViewCell.swift
@@ -24,6 +24,30 @@ class RecipeTableViewCell: UITableViewCell {
         recipeImage.image = UIImage(named: "Spaghetti-bolognaise")
         titleLabel.text = ""
         ingredientsLabel.text = ""
+        
+   
        }
+    
+    // ADD THE GRADIENT LAYER ON THE RECIPE IMAGE
+    override func layoutSublayers(of layer: CALayer) {
+        
+        let width = self.bounds.width
+        let height = self.bounds.height
+        let sHeight:CGFloat = 60.0
+        
+        if recipeImage.layer.sublayers?.first is CAGradientLayer {
+            }
+        else {
+            let gradient = CAGradientLayer()
+            gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+            gradient.frame = CGRect(x: 0, y: height - sHeight, width: width, height: sHeight)
+            recipeImage.layer.insertSublayer(gradient, at: 0)
+        }
+    }
 
+   
+      
+    
 }
+
+
