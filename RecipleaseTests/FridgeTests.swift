31e7820878223290ae27788c975a84634eae71c4
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-07-07 11:06:35 +0200

    Create tests for Fridge class

--- RecipleaseTests/FridgeTests.swift
+++ RecipleaseTests/FridgeTests.swift
@@ -6,30 +6,34 @@
 //
 
 import XCTest
+@testable import Reciplease
+
 
 final class FridgeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var fridge = Fridge()
+    
+    
+    func testRemoveAllIngredients() {
+        fridge.ingredients = ["Cheese", "Chicken"]
+        
+        fridge.removeAllIngredients()
+        
+        XCTAssertTrue(fridge.ingredients.isEmpty)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAddIngredient() {
+        fridge.addIngredient(ingredient: "Cheese")
+        
+        XCTAssertEqual(fridge.ingredients.count, 1)
+        XCTAssertEqual(fridge.ingredients[0], "Cheese")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testIngredientsString() {
+        fridge.ingredients = ["Cheese Chicken", "Bread"]
+        
+        let ingredientsString = fridge.ingredientsString()
+        
+        XCTAssertEqual(ingredientsString, "&q=Cheese%20+Chicken%20+Bread")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
